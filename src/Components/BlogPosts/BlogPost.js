dac0b699bc46fb78f8359d4bf95b89e26b8be602
Rajib Sarker <redhat0593@gmail.com>
2023-03-04 20:32:53 +0600

    cards for blog data craeated completed

--- src/Components/BlogPosts/BlogPost.js
+++ src/Components/BlogPosts/BlogPost.js
@@ -1,11 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Blogpost.css';
 import { Container } from 'react-bootstrap';
 import { FaCaretDown } from 'react-icons/fa';
-import addp from './../../assets/images/addg.png'
+import addp from './../../assets/images/addg.png';
+import im1 from './../../assets/images/1im3.png';
+import im2 from './../../assets/images/2im1.png';
+import im3 from './../../assets/images/3im.png';
+import av1 from './../../assets/images/av1.png';
+import av2 from './../../assets/images/av2.png';
+import av3 from './../../assets/images/av3.png';
+import av4 from './../../assets/images/av4.png';
+import PostCard from './PostCard/PostCard';
+
+
+const blogData = [
+    {
+        image: im1,
+        type: "✍️ Article",
+        heading: "What if famous brands had regular fonts? Meet RegulaBrands!",
+        details: "I’ve worked in UX for the better part of a decade. From now on, I plan to rei…",
+        writer: "Sarthak Kamra",
+        avatar: av1,
+        view: "1.4"
+    },
+    {
+        image: im2,
+        type: "🔬️ Education",
+        heading: "Tax Benefits for Investment under National Pension Scheme launched by Government",
+        details: "I’ve worked in UX for the better part of a decade. From now on, I plan to rei…",
+        writer: "Sarah West",
+        avatar: av2,
+        view: "1.4"
+    },
+    {
+        image: im3,
+        type: "🗓️ Meetup",
+        heading: "Finance & Investment Elite Social Mixer @Lujiazui",
+        writer: "Ronal Jones",
+        avatar: av3,
+        view: "1.4",
+        date: "Fri, 12 Oct, 2018",
+        location: "Ahmedabad, India",
+        website: "https://rajib-sarker-portfolio.netlify.app/"
+    },
+    {
+        type: "💼️ Job",
+        heading: "Software Developer",
+        writer: "Joseph Gray",
+        avatar: av4,
+        view: "1.4",
+        company: "Innovaccer Analytics Private Ltd.",
+        location: "Noida, India",
+        job: "https://rajib-sarker-portfolio.netlify.app/"
+    },
+]
 
 
 const BlogPost = () => {
+    const [data, setdata] = useState(blogData);
+
     return (
         <div className='mt-5 blog'>
             <Container>
@@ -23,8 +76,13 @@ const BlogPost = () => {
                     </div>
                 </div>
                 <hr className='mb-4 mt-1' />
-                <div className="d-flex">
-                    <div className="cards"></div>
+                <div className="d-flex contents">
+                    <div className="cards">
+                        {
+                            data?.map((item, i) => <PostCard key={i} data={item} />)
+                        }
+                    </div>
+                    <div className="search-opt"></div>
                 </div>
             </Container>
         </div>
--- src/Components/BlogPosts/PostCard/PostCard.js
+++ src/Components/BlogPosts/PostCard/PostCard.js
@@ -1,9 +1,86 @@
 import React from 'react';
+import './PostCard.css';
+import sharebt from './../../../assets/images/share.png';
+import eye from './../../../assets/images/eye.png';
+import dot from './../../../assets/images/dot.png';
+import { FaCalendarAlt, FaMapMarkerAlt, FaBriefcase } from 'react-icons/fa';
+import { Dropdown } from 'react-bootstrap';
 
-const PostCard = () => {
+
+const PostCard = ({ data }) => {
+    const {
+        image,
+        type,
+        heading,
+        details,
+        writer,
+        avatar,
+        view,
+        date,
+        location,
+        website,
+        job,
+        company
+    } = data;
+
     return (
-        <div>
+        <div className='card-body'>
+            <div className="image">
+                {
+                    image && <img src={image} alt="images" className="img-fluid" />
+                }
+            </div>
+            <div className="text">
+                <p className='type m-0'>{type}</p>
+                <div className="d-flex justify-content-between">
+                    <h2 className='heading'>{heading}</h2>
+                    <Dropdown>
+                        <Dropdown.Toggle variant="success" id="dropdown-basic">
+                            <img src={dot} alt="sd" />
+                        </Dropdown.Toggle>
 
+                        <Dropdown.Menu>
+                            <Dropdown.Item href="#/action-1">Edit</Dropdown.Item>
+                            <Dropdown.Item href="#/action-2">Report</Dropdown.Item>
+                            <Dropdown.Item href="#/action-3">Oops !!</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </div>
+                {
+                    details && <p className="details m-0">{details}</p>
+                }
+                <div className="d-flex">
+                    <div>
+                        {date && <p className="date m-0"><FaCalendarAlt className='me-2' /> {date}</p>}
+                        {company && <p className="company"><FaBriefcase className='me-2' /> {company}</p>}
+                    </div>
+                    <div>
+                        {location && <p className="location"><FaMapMarkerAlt className='me-1' /> {location}</p>}
+                    </div>
+                </div>
+                <div>
+                    {
+                        website && <a className='web-btn red w-100' href={website}>Visit Website</a>
+                    }
+                </div>
+                {
+                    job && <a className='web-btn green w-100' href={job}>Apply on Timesjobs</a>
+                }
+                <div className="d-flex justify-content-between">
+                    <div className='d-flex align-items-center'>
+                        {
+                            avatar && <img src={avatar} alt="avatar" className='me-2' />
+                        }
+                        {
+                            writer && <p className='writter m-0'>{writer}</p>
+                        }
+                    </div>
+                    <div className='d-flex align-items-center'>
+                        <p className='view'><img className='me-2' src={eye} alt="eye" />{view}k views</p>
+                        <a href="/" className="sharebtn"><img className='flex-shrink-0' src={sharebt} alt="s" /></a>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
